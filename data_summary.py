1af767979c0b9915faf6c1507360e31317b1afca
Aleksandra Ziobrowska <olaziobrowska1@gmail.com>
2020-10-22 22:39:32 +0200

    Correlation matrix

--- data_summary.py
+++ data_summary.py
@@ -8,6 +8,16 @@ df = pd.read_csv(os.path.join('data', 'concatenated_source_data_heart_disease.cs
 described = df.describe().round(3)
 print(described)
 
+# Check for NA
+print(df.isnull().sum().sum())
+
+# Correlation matrix
+c = df.corr().abs()
+
+s = c.unstack()
+so = s.sort_values(kind="quicksort")
+print(so)
+
 #df.plot(kind='scatter', x='Age', y='Pain location')
 df.groupby(['disease'])['disease'].agg(['count']).plot.pie(y='count')
 plt.show()
